11300988eec4aa63481813908336c5ae5eccbbce
Modyzone <mody.zone@outlook.com>
2021-08-10 16:28:20 +0200

    add new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-let myWord = 'Hello World';
-alert(myWord);
-let myFavoritfood= 'Chicken';
-document.write(myFavoritfood);
+let grassPokemon = [ {name: "balbasure", height: 7, types: ['grass','poison'] },
+{name: "ivysaur", height: 1, types: ['grass','posion'] } ];
+
+let firePokemon = [ {name: "charmander", height: 0.6, types: 'fire'}, 
+{name: "charmeleon", height: 1.1, types: 'fire'} ];
+
+let waterPokemon = [ {name: "squirtle", height: 0.5, types: 'water'},
+{name: "wartortle", height: 1, types: 'water'} ];
+
+console.log(grassPokemon);
+console.log(firePokemon);
+console.log(waterPokemon);
