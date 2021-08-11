099601607dc09cf33c800c0826f4e57b140d4ffe
Modyzone <mody.zone@outlook.com>
2021-08-11 11:18:08 +0200

    add new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
-let grassPokemon = [ {name: "balbasure", height: 7, types: ['grass','poison'] },
-{name: "ivysaur", height: 1, types: ['grass','posion'] } ];
+let pokemonList = [ {name: "balbasure", height: 7, types: ['grass','poison'] },
+{name: "ivysaur", height: 1, types: ['grass','posion'] },
+{name: "charmander", height: 0.6, types: 'fire'},
+{name: "charmeleon", height: 1.1, types: 'fire'},
+{name: "squirtle", height: 0.5, types: 'water'},
+{name: "wartortle", height: 1, types: 'water'}];
 
-let firePokemon = [ {name: "charmander", height: 0.6, types: 'fire'}, 
-{name: "charmeleon", height: 1.1, types: 'fire'} ];
-
-let waterPokemon = [ {name: "squirtle", height: 0.5, types: 'water'},
-{name: "wartortle", height: 1, types: 'water'} ];
-
-console.log(grassPokemon);
-console.log(firePokemon);
-console.log(waterPokemon);
+console.log(pokemonList);
