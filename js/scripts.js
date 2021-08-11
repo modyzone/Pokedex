48145581de6a7b1c4cf541908ce2c5088bb19cd3
Modyzone <mody.zone@outlook.com>
2021-08-11 11:42:39 +0200

    add new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let pokemonList = [ {name: "balbasure", height: 7, types: ['grass','poison'] },
 {name: "ivysaur", height: 1, types: ['grass','posion'] },
-{name: "charmander", height: 0.6, types: 'fire'},
-{name: "charmeleon", height: 1.1, types: 'fire'},
-{name: "squirtle", height: 0.5, types: 'water'},
-{name: "wartortle", height: 1, types: 'water'}];
+{name: "charmander", height: 0.6, types: ['fire'] },
+{name: "charmeleon", height: 1.1, types: ['fire'] },
+{name: "squirtle", height: 0.5, types: ['water'] },
+{name: "wartortle", height: 1, types: ['water'] } ];
 
 console.log(pokemonList);
