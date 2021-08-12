c63ac4f9a212b9217568a53f2326dff230a30f6f
Modyzone <mody.zone@outlook.com>
2021-08-12 21:01:11 +0200

    add new files

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,35 @@ let pokemonList = [ {name: "balbasure", height: 7, types: ['grass','poison'] },
 {name: "wartortle", height: 1, types: ['water'] } ];
 
 console.log(pokemonList);
+
+let pokemon = [
+{
+	name: "Bulbasaur",
+	height: 0.7,
+	types: ["grass", "poison"]
+},
+{
+	name: "Charizard" ,
+	height: "1.7",
+	types: ["fire", "flying"]
+},
+{
+	name: "Squirtie",
+	height: 1,
+	types: ["water"]
+}
+];
+for (let i = 0; i < pokemon.length; i++);
+
+	if (pokemon[i].height > 1) {
+
+		document.write("<p> " +pokemon[i].name +" "+ pokemon[i].height, " Wow! that is big! </p> <br/>");
+
+	} else if (pokemon[i].height < 1) {
+
+		document.write("<p> " +pokemon[i].name +" "+ pokemon[i].height, " Average Pokemon! </p> <br/>");
+
+	} else { 
+
+		document.write("<p> "+pokemon[i].name +" "+ pokemon[i].height," Small Pokemon! </p> <br/>")
+	}
