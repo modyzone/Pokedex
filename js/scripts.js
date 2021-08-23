24592fcc0e6c1c3324dcee8f86a1ce684c118a13
Modyzone <mody.zone@outlook.com>
2021-08-23 16:02:16 +0200

    add new files

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,39 @@ var pokemonRepository = (function () {
 	function getAll() {
 		return pokemonList;
 	}
-	function add(pokemon) {
+    
+    function add(pokemon) {
 		pokemonList.push(pokemon);
+	}
+
+	function addListItem(pokemon) {
+	let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+	pokemonList.appendChild(listpokemon);
 	}
 	return {
 		getAll: getAll,
-		add: add
+		add: add,
+		addListItem: addListItem
 	};
 }) ();
-pokemonRepository.getAll().forEach (function(pokemon) { 
-console.log(pokemon.name + ' is ' + pokemon.height + ' meter height.' )
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function (pokemon) {
+	pokemonRepository.addListItem(pokemon);
+	
 });
+
+
+
+
+
+
+
+
+//console.log(pokemon.name + ' is ' + pokemon.height + ' meter height.' )
